3dfea35b01d51f8c3eeccf76c734a469998a8f74
Xifre Font <xifre.font.97@gmail.com>
2022-07-16 13:56:48 +0200

    Fixed error in cell rules

--- public/js/cellRules/cellRules.js
+++ public/js/cellRules/cellRules.js
@@ -3,23 +3,31 @@ import updateCell from "../gridUpdater/updateGrid.js";
 import gridReader from "../gridReader/gridReader.js";
 
 const cellRules = (status, row, column, neighbours) => {
-  if (neighbours < gameData.rules.minCellsToDie && status === 1) {
+  if (neighbours < gameData.rules.minCellsToDie) {
     updateCell(row, column, 0);
+    return;
   }
 
   if (
     neighbours === gameData.rules.cellsToLive[0] ||
-    (neighbours === gameData.rules.cellsToLive[1] && status === 1)
+    neighbours === gameData.rules.cellsToLive[1]
   ) {
-    updateCell(row, column, 1);
+    if (status === 1) {
+      updateCell(row, column, 1);
+      return;
+    }
   }
 
-  if (neighbours === gameData.rules.cellsToBeBorn && status === 0) {
+  if (neighbours === gameData.rules.cellsToBeBorn) {
     updateCell(row, column, 1);
+    return;
   }
 
-  if (neighbours > gameData.rules.maxCellsToDie && status === 1) {
-    updateCell(row, column, 0);
+  if (neighbours > gameData.rules.maxCellsToDie) {
+    if (status === 1) {
+      updateCell(row, column, 0);
+      return;
+    }
   }
 };
 
